a6adaea29be1073ce054af74a4d4ba012f02aa49
Cyril "Nick" Engmann <cyengmann@gmail.com>
2024-03-20 13:49:32 -0400

    Update custom_logger.py
    Filter binary data

--- custom_logger.py
+++ custom_logger.py
@@ -5,8 +5,10 @@ class BinaryFilter(logging.Filter):
     def filter(self, record):
         # Check if the log message contains binary data
         if isinstance(record.msg, bytes):
+            return False  # Skip logging binary data
+        elif "STREAM" in record.msg:
             return False  # Skip logging binary data to the console
-        return True  # Allow logging text data to the console
+        return True  # Allow logging text data
 
 def get_logger(log_file: str, repo_url: str) -> logging.Logger:
     # Create a custom formatter
@@ -17,11 +19,16 @@ def get_logger(log_file: str, repo_url: str) -> logging.Logger:
     # Create a file handler
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
+    # Create an instance of BinaryFilter for the file handler
+    file_binary_filter = BinaryFilter()
+    # Add the BinaryFilter to the file handler
+    file_handler.addFilter(file_binary_filter)
 
     # Create a console handler
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    console_handler.addFilter(BinaryFilter())
+    console_binary_filter = BinaryFilter()  # Create an instance of the filter for console
+    console_handler.addFilter(console_binary_filter)  # Add the filter to the console handler
 
     # Create a logger and set the log level
     logger = logging.getLogger()
